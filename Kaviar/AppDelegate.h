1ab36b69fbcf8779fbc93d4a26288d58850a183b
Tapan Srivastava <destinypenguin101@gmail.com>
2016-05-16 15:16:20 -0700

    Initial Commit

---
+++ Kaviar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kaviar
+//
+//  Created by Tapan Srivastava on 5/16/16.
+//  Copyright © 2016 Institute of Systems Biology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kaviar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Kaviar
+//
+//  Created by Tapan Srivastava on 5/16/16.
+//  Copyright © 2016 Institute of Systems Biology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
