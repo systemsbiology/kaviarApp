ea6f91949639fb8e5a6083ea16e39cc91b815160
Tapan Srivastava <tapansriv@gmail.com>
2016-06-01 13:41:00 -0700

    Revert "first commit"
    This reverts commit e528bbd2e36241e492db3fc201f46ab36bc479af.

--- Kaviar/ParameterEntryViewController.h
+++ Kaviar/ParameterEntryViewController.h
@@ -34,7 +34,6 @@
 @property (strong, nonatomic) IBOutlet UILabel *outputLabel;
 @property (strong, nonatomic) IBOutlet UILabel *variantTypes;
 @property (strong, nonatomic) IBOutlet UIButton *submitLabel;
-@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
 
 - (IBAction)submit:(id)sender;
 
